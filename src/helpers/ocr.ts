3b79fcd407e046bd78ac4ef4aa8b0d9db35bc6ae
John Lowther <jw.lowther@hotmail.co.uk>
2024-11-09 23:36:45 +0000

    Better text scanning

--- src/helpers/ocr.ts
+++ src/helpers/ocr.ts
@@ -1,19 +1,47 @@
-import { RecognizeResult } from "tesseract.js";
+import { RecognizeResult, Line, Word } from "tesseract.js";
+
+type ParsingContext = {
+  html: string;
+  lastIndent: number;
+}
 
 export const parseTesseractResult = (result: RecognizeResult) => {
-  let paragraph = '<p>';
-  let lineStart = 0;
-  console.log(result)
+  const context = createInitialContext();
   for (const line of result.data.lines) {
-    if (line.baseline.x0 > lineStart + 50 || line === line.paragraph.lines[0]) {
-      if (paragraph !== '') paragraph += '</p>';
-      paragraph += '<p>';
-    }
-    lineStart = line.baseline.x0;
-    for (const word of line.words) {
-      paragraph += word.text + ' ';
-    }
+    processLine(line, context);
   }
-  paragraph += '</p>';
-  return paragraph;
+  return context.html;
+}
+
+const processLine = (line: Line, context: ParsingContext) => {
+  const indent = line.bbox.x0;
+
+  const isStartOfSentence = /^[^a-zA-Z0-9]*[A-Z]/.test(line.text);
+  const isParagraph = indent > context.lastIndent + 10 && isStartOfSentence;
+  if (isParagraph) {
+    context.html += "</p><p>";
+  } else {
+    context.lastIndent = indent;
+  }
+
+  for (const word of line.words) {
+    processWord(word, context);
+  }
+
+  if (line.text.endsWith("\n\n")) {
+    context.html += "</p><p>";
+  }
+}
+
+const processWord = (word: Word, context: ParsingContext) => {
+  const isJoinedWord = word.text.endsWith("-");
+  const wordText = isJoinedWord ? word.text.slice(0, word.text.length - 1) : word.text;
+  context.html += wordText + (isJoinedWord ? "" : " ");
+}
+
+const createInitialContext = (): ParsingContext => {
+  return {
+    html: "",
+    lastIndent: Infinity
+  }
 }
